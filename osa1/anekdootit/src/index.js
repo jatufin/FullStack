f56e5afcf37d48f115052e34f2505e2b142f9b22
Janne Tuukkanen <janne@ibfekna.com>
2020-11-24 06:22:14 +0200

    parts 1.12, 1.13 and 1.14

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,12 +1,70 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const MostVoted = ({votes, anecdotes}) => {
+  let n=0
+
+  for(let i=0 ; i < votes.length; i++) {
+    if(votes[i] > votes[n]) {
+      n=i
+    }
+  }
+
+  // If no votes has been cast, show nothing
+  if (votes[n] === 0) {
+    return (<div></div>)
+  }
+
+  return(
+    <div>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[n]}</p>
+      <p>has {votes[n]} votes</p>
+    </div>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState([])
 
+  const initVotes = () => {
+    if(votes.length === anecdotes.length) {
+      return
+    }
+
+    let a = new Array(anecdotes.length)
+
+    for(let i=0;i<anecdotes.length;i++) {
+      a[i] = 0
+    }
+
+    setVotes(a)
+  }
+
+  const randomAnecdote = () => {
+    const n = Math.floor((Math.random() * anecdotes.length))
+    setSelected(n)
+  }
+
+  const voteCurrent = () => {
+    let v = [...votes]
+    v[selected]++
+    setVotes(v)
+
+    console.log(v)
+  }
+
+  initVotes()
+  
   return (
     <div>
-      {props.anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={voteCurrent}>Vote</button>
+      <button onClick={randomAnecdote}>Next anecdote</button>
+      <MostVoted votes={votes} anecdotes={props.anecdotes} />
     </div>
   )
 }
