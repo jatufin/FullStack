1883be4ca5e94bdff7a16912099a8a1e8d5651f9
Janne Tuukkanen <janne@ibfekna.com>
2020-11-27 02:37:15 +0200

    part 2.12

---
+++ osa2/maidentiedot/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import CountryList from './CountryList'
+
+
+const App = () => {
+  const [countries , setCountries] = useState([])
+  const [filter, setFilter] = useState("")
+
+  const filteredCountries = countries.filter(country =>
+    country.name.toUpperCase().includes(filter.toUpperCase())
+  )
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then(response =>
+        setCountries(response.data))
+  }, [])
+
+  const filterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  
+  return (
+    <div>
+      <input value={filter} onChange={filterChange} />
+      <CountryList countries={filteredCountries} />
+    </div>
+  )
+}
+
+export default App
---
+++ osa2/maidentiedot/src/CountryList.js
@@ -0,0 +1,51 @@
+const CountryList = ({countries}) => {
+    if(countries.length === 0) {
+      return(
+        <div>
+          <p>No countries found</p>
+        </div>
+      )
+    }
+    if(countries.length > 10) {
+      return(
+        <div>
+          <p>Too many matches, specify another filter</p>
+        </div>
+      )
+    }
+    
+    if(countries.length > 1) {
+      return(
+        <div>
+          {countries.map(country =>
+            <p key={country.alpha3Code}>{country.name}</p>
+          )}
+        </div>
+      )
+    }
+  
+    return(
+      <div>
+        <CountryInfo country={countries[0]} />
+      </div>
+    )
+  }
+  
+  const CountryInfo = ({country}) => {
+    return(
+      <div>
+        <h1>{country.name}</h1>
+          <p>capital {country.capital}</p>
+          <p>population {country.population}</p>
+          <h2>languages</h2>
+            <ul>
+              {country.languages.map(language =>
+                <li key={language.iso639_2}>{language.name}</li>
+              )}
+            </ul>
+          <img src={country.flag} alt="flag" width="150" />
+      </div>
+    )
+  }
+
+  export default CountryList
