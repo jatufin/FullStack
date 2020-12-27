3046ff4330fcf64e9fb437ad9a08df090113d8c2
Janne Tuukkanen <janne@ibfekna.com>
2020-12-27 11:53:42 +0200

    part 6.10

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -33,7 +33,7 @@ export const createAnecdote = (content) => {
   }
 }
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
@@ -51,4 +51,4 @@ const reducer = (state = initialState, action) => {
   
 }
 
-export default reducer
+export default anecdoteReducer
---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,10 @@
+const notificationAtStart = 'Initial notification'
+
+const notificationReducer = (state = notificationAtStart, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default notificationReducer
