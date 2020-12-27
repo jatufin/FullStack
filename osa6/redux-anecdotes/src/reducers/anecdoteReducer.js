7139e8daa0164c063849f9733e7029c6da293d56
Janne Tuukkanen <janne@ibfekna.com>
2020-12-27 04:24:09 +0200

    part 6.3

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,11 +19,27 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
+export const voteDispatchObject = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
+}
+
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      return state.map(anecdote => anecdote.id !== action.data.id
+        ? anecdote
+        : { ...anecdote, votes: anecdote.votes + 1})
+    default:
+      return state
+  }
+  
+  
 }
 
 export default reducer
