44ee3645a42a4e6b7b0e2da289b1d94b95045961
Janne Tuukkanen <janne@ibfekna.com>
2020-12-28 21:51:08 +0200

    fix a bug displaying notification about added anecdote

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -14,7 +14,7 @@ const AnecdoteForm = () => {
     event.target.anecdote.value = ''
     dispatch(createAnecdote(content))
     
-    dispatch(setNotification(`You added anecdote '${content}'`), 5)
+    dispatch(setNotification(`You added anecdote '${content}'`, 5))
   }
 
   return(
