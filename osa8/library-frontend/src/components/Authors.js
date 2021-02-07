d35c0e17cb015967186e8081d053d4e1b4eda46c
Janne Tuukkanen <janne@ibfekna.com>
2021-02-07 13:38:02 +0200

    fix part 8.8

--- osa8/library-frontend/src/components/Authors.js
+++ osa8/library-frontend/src/components/Authors.js
@@ -1,7 +1,27 @@
   
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
 
-const Authors = ({ authors }) => {
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS)
+
+  if(!props.show) {
+    return null
+  }
+  if(result.loading) {
+    return <div>loading...</div>
+  }
+
   return (
     <div>
       <h2>authors</h2>
@@ -16,7 +36,7 @@ const Authors = ({ authors }) => {
               books
             </th>
           </tr>
-          {authors.map(a =>
+          {result.data.allAuthors.map(a =>
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
